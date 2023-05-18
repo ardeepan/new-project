1c57dd2d07e1b4b875513a3c9d936d6738680c52
Deepanshu <ardeepan@sheridancollege.ca>
2023-05-18 15:16:32 -0400

    new project uploaded

---
+++ src/average/Average.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package average;
+
+/**
+ *
+ * @author Deepanshu Arora,2023
+
+ */
+public class Average {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+	
+	double answer = 0;
+		for(int i=0; i<args.length; i++) {
+		String arg = args[i];
+      	 answer += Double.parseDouble(arg);
+	
+	
+		}
+		System.out.println("Average is "+answer/args.length);
+	}
+
+    }
+
+
